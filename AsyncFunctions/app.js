19e46881cb90d1fa1928b0f976ea014af2fc3ec4
Brandon Dalton <dabigin@gmail.com>
2023-04-19 12:49:14 -0500

    Section 17 Lecture 284 The Await Keyword

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -16,18 +16,73 @@
 //     console.log(err);
 //   });
 
-const login = async (username, password) => {
-  if (!username || !password) throw "Missing Credentials";
-  if (password === "corgifeetarecute") return "WELCOME!";
-  throw "Invalid Password";
+// const login = async (username, password) => {
+//   if (!username || !password) throw "Missing Credentials";
+//   if (password === "corgifeetarecute") return "WELCOME!";
+//   throw "Invalid Password";
+// };
+
+// login("akjsdhsa", "corgifeetarecute")
+//   .then((msg) => {
+//     console.log("LOGGED IN!");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR!");
+//     console.log(err);
+//   });
+
+// const delayedColorChange = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
+
+// delayedColorChange("red", 1000)
+//   .then(() => delayedColorChange("orange", 1000))
+//   .then(() => delayedColorChange("yellow", 1000))
+//   .then(() => delayedColorChange("green", 1000))
+//   .then(() => delayedColorChange("blue", 1000))
+//   .then(() => delayedColorChange("indigo", 1000))
+//   .then(() => delayedColorChange("violet", 1000));
+
+// async function rainbow() {
+//   await delayedColorChange("red", 1000);
+//   await delayedColorChange("orange", 1000);
+//   await delayedColorChange("yellow", 1000);
+//   await delayedColorChange("green", 1000);
+//   await delayedColorChange("blue", 1000);
+//   await delayedColorChange("indigo", 1000);
+//   await delayedColorChange("violet", 1000);
+//   return "ALL DONE!";
+// }
+
+// rainbow().then(() => console.log("END OF RAINBOW!"));
+
+// async function printRainbow() {
+//   await rainbow();
+//   console.log("END OF RAINBOW!");
+// }
+
+// printRainbow();
+
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
 };
 
-login("akjsdhsa", "corgifeetarecute")
-  .then((msg) => {
-    console.log("LOGGED IN!");
-    console.log(msg);
-  })
-  .catch((err) => {
-    console.log("ERROR!");
-    console.log(err);
-  });
+async function makeTwoRequests() {
+  let data1 = await fakeRequestPromise("/page1");
+  console.log(data1);
+}
